b4140c46f1abbfb3ed2f883a3af5e17ca0a5a6e1
anzhelika.shaiko <dannasangell@gmail.com>
2023-05-10 17:08:24 +0300

    Implemented logic of character rotation

---
+++ UnityCharacterCreation/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    private Vector2 touchStartPos;
+    private float rotationSpeed = 0.1f;
+    private float damping = 0.2f;
+
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+
+    void Update() {
+        if (Input.touchCount == 1) {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began) {
+                touchStartPos = touch.position;
+            } else if (touch.phase == TouchPhase.Moved) {
+                Vector2 currentTouchPos = touch.position;
+                Vector2 touchDelta = currentTouchPos - touchStartPos;
+
+                rotationX += touchDelta.x * rotationSpeed * damping;
+                rotationY -= touchDelta.y * rotationSpeed * damping;
+
+                transform.rotation = Quaternion.Euler(rotationY, rotationX, 0f);
+            }
+        }
+    }
+}
